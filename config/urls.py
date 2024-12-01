10bd37ad92fcaebe80ae3ffa581d96b985860c14
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-12-01 17:31:27 +0300

    urls created

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user.views import  check_otp, login_page, generate_otp, profile_page, enter_invite_code, logout_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('check/', check_otp, name="check_otp"),
+    path('login/', login_page, name="login"),
+    path('', logout_view, name="logout"),
+    path('otp/<int:pk>/<uuid>/', generate_otp),
+    path('<str:invite_code>/', profile_page, name="profile"),
+    path('<str:invite_code>/enter_invite_code/', enter_invite_code, name="enter_invite_code"),
+
 ]
