1e363d0a07e4b547c67ce37be5037c3269fe41fa
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-12-01 22:18:07 +0300

    Serializer for api created

---
+++ user/api_serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+	"""Сериализатор для объекта модели User"""
+	invited_followers = serializers.SerializerMethodField()
+	phone = serializers.SerializerMethodField()
+
+	@staticmethod
+	def get_phone(instance):
+		return str(instance.phone)
+
+	@staticmethod
+	def get_invited_followers(instance):
+		return [str(user) for user in instance.user_set.all()]
+
+	class Meta:
+		model = User
+		fields = ["id", "phone", "invite_code", "invited_by", "invited_followers"]
